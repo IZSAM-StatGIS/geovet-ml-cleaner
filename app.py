61e8967bc6475d98f9a7bd356435eb989daede22
Alessio Di Lorenzo <a.dilorenzo@izs.intra>
2023-03-30 10:49:13 +0200

    Modifica label markdown

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.markdown("""
     1) crea campo "da_sito" in *statgis_latest.xlsx* (da_sito = 'NO') e *udanet_latest.csv* (da_sito = 'YES')
     2) elimina i doppioni dai due file citati nel punto precedente 
     3) concatena e pulisce il dato concatenato dai doppioni, tenendo l'occorrenza presente in *statgis_latest.xlsx* (quindi quella che contiene tutte le info oltre all'indirizzo email)
-    3) rimuove dal dato concatenato tutti gli indirizzi presenti nel file *unsubscribe_latest.xlsx*
+    3) rimuove dal dato concatenato tutti gli indirizzi presenti nel file *unsubscribe_latest.xls*
     4) mostra a schermo il dato pulito sotto forma di tabella e permette di scaricarlo in formato excel 
 
             """)
